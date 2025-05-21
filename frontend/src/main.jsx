79654e800941939a60c83a894d490a815738b8d5
Kalany <duncan.miard@outlook.fr>
2025-05-21 05:19:49 +0200

    Ajout Router main & préparation structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./style/Main.scss";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/article/:id" element={<Article />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
