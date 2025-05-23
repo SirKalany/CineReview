054ada528b649348ad340facb92dc5ba32b09b8d
Kalany <duncan.miard@outlook.fr>
2025-05-23 04:57:26 +0200

    Paramétrage backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 app.listen(port, () => {
